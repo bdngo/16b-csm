54711a90fda08c1bf012875b5c30074a40750539
Bryan Ngo <bryanngo@berkeley.edu>
2022-08-19 10:36:58 -0700

    bulk sp22 push

---
+++ sp22/2022-03-29/gram_schmidt.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def proj(x, y):
+    return np.inner(x, y) / np.inner(x, x) * x
+
+
+def gram_schmidt(V):
+    U = []
+    for v in V:
+        q = v
+        for u in U:
+            q -= proj(u, q)
+        U.append(q / np.linalg.norm(q))
+    return np.array(U)
+
+
+def main():
+    A = np.array([
+        [-1, 1, -1, 1],
+        [-1, 3, -1, 3],
+        [1, 3, 5, 7]
+    ], dtype=float)
+    U = gram_schmidt(A)
+
+    # print(A)
+    print(U)
+
+
+if __name__ == "__main__":
+    main()
